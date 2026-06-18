1f13cb2ac0ac0775b04247384ec58785d1e07fd7
hanseungbeom <hsb9838@naver.com>
2026-06-18 10:11:19 +0900

    Week 14: 404 예외 처리 및 vercel.json 세팅 완료

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import MovieDetailView from "../views/MovieDetailView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -20,6 +21,9 @@ const router = createRouter({
       name: "movie-detail",
       component: MovieDetailView,
     },
+    { path: "/movies/:id", name: "movie-detail", component: MovieDetailView },
+
+    { path: "/:pathMatch(.*)*", name: "not-found", component: NotFoundView },
   ],
 
   scrollBehavior(to, from, savedPosition) {
